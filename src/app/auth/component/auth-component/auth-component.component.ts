fa3b03e1b827df2a84de1bba4931f7ecfd22f772
cotaByte <minanajoverrafa@gmail.com>
2024-07-20 17:36:58 +0200

    refact/ improve auth component

--- src/app/auth/component/auth-component/auth-component.component.ts
+++ src/app/auth/component/auth-component/auth-component.component.ts
@@ -7,7 +7,15 @@ import {
   Validators,
 } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Subject, map, shareReplay, startWith, switchMap, tap } from 'rxjs';
+import {
+  Observable,
+  Subject,
+  map,
+  shareReplay,
+  startWith,
+  switchMap,
+  tap,
+} from 'rxjs';
 import { AuthServiceService } from '../../services/auth-service.service';
 import { CardModule } from 'primeng/card';
 import { Button, ButtonModule } from 'primeng/button';
@@ -32,7 +40,7 @@ export class AuthComponentComponent {
   //#endregion AUTH FORM
 
   //#region FORM VALIDATOR
-  public authFormValid$ = this.authForm.statusChanges.pipe(
+  public authFormValid$: Observable<boolean> = this.authForm.statusChanges.pipe(
     map(status => status === 'VALID'),
     startWith(false),
     shareReplay({ bufferSize: 1, refCount: true })
@@ -41,16 +49,14 @@ export class AuthComponentComponent {
 
   //#region ON AUTH
   public auth = new Subject<void>();
-  public readonly onAuth$ = this.auth
-    .pipe(
-      switchMap(() => this.authService.logIn(this.authForm.getRawValue())),
-      tap(res => {
-        console.log('res ', res.ok);
-        res.ok && localStorage.setItem('user', res.ouser);
-        res.ok && this.route.navigate(['home']);
-      })
-    )
-    .subscribe();
+  public readonly onAuth$ = this.auth.pipe(
+    switchMap(() => this.authService.logIn(this.authForm.getRawValue())),
+    tap(res => {
+      console.log('res ', res.ok);
+      res.ok && localStorage.setItem('user', res.ouser);
+      res.ok && this.route.navigate(['home']);
+    })
+  );
   //#endregion ON AUTH
 
   constructor(public route: Router, public authService: AuthServiceService) {}
