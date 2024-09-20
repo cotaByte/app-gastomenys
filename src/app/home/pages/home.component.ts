7952c7da5fe5ba54d089b8312386004dd455d4a9
cotaByte <minanajoverrafa@gmail.com>
2024-09-20 17:10:12 +0200

    feat: Add gasto-item component and update home page

--- src/app/home/pages/home.component.ts
+++ src/app/home/pages/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 import { PieChartExpensesComponent } from '../components/pie-chart-expenses/pie-chart-expenses.component';
+import { GastoItemComponent } from '../../shared/components/gasto-item/gasto-item.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, PieChartExpensesComponent],
+  imports: [NavbarComponent, PieChartExpensesComponent, GastoItemComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
